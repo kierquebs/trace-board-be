9ffb5f4ca5a0df3ea4891552aa035e79c9f0f6e4
Kier Quebral <40307377+kierquebs@users.noreply.github.com>
2026-03-27 12:52:43 +0800

    fix: add port 3000 to CORS allowed origins in dev (#6)

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -45,6 +45,8 @@ CELERY_RESULT_BACKEND = REDIS_URL
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
 ]
 CORS_ALLOW_CREDENTIALS = True
 
